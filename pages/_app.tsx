79fe8233c5a52aead64bce014153d65cd32647e3
evribsap <evribsaputra@gmail.com>
2022-10-02 01:02:40 +0700

    add pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,48 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable @next/next/no-sync-scripts */
+import '../styles/utilities.css';
+import '../styles/homepage.css';
+import '../styles/detail.css';
+import '../styles/checkout.css';
+import '../styles/complete-checkout.css';
+import '../styles/sign-in.css';
+import '../styles/sign-up.css';
+import '../styles/sign-up-photo.css';
+import '../styles/sign-up-photo-success.css';
+import '../styles/sign-up-success.css';
+import '../styles/404-not-found.css';
+import '../styles/overview.css';
+import '../styles/sidebar.css';
+import '../styles/transactions.css';
+import '../styles/transactions-detail.css';
+import '../styles/edit-profile.css';
+
+
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* Bootstrap CSS */}
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossOrigin="anonymous"
+        />
+
+        {/* Google Font */}
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+
+        {/* Bootstrap JS */}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+        </script>
+
+        
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
